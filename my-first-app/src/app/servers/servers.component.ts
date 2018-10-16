e9af47bed6c73f7fa343799244717a4c0b2ca2d1
Volodymyr Kulibaba <kvladimir2007@gmail.com>
2018-10-16 12:29:07 +0300

    Added servers, server, warning and success components

---
+++ my-first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // selector: 'app-servers',
+  // selector: '[app-servers]',
+  selector: '.app-servers',
+  /* templateUrl: './servers.component.html', */
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>
+  `,
+  /* styleUrls: ['./servers.component.css'] */
+  styles: [`
+    p {
+      color: dodgerblue;
+    }
+  `]
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
